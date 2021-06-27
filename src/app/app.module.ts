ad63b6594dc4cb659de31850466b160aa62fbb76
wilsonkao <wilkao036@gmail.com>
2021-06-27 15:16:08 +0800

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { FormsModule } from '@angular/forms';
+import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DialogModule } from 'primeng/dialog';
 import { AppComponent } from './app.component';
+import { CreateComponent } from './create/create.component';
+import { DetailDialogComponent } from './detail-dialog/detail-dialog.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CreateComponent, DetailDialogComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule,
+    RippleModule,
+    FormsModule,
+    TableModule,
+    InputTextModule,
+    ConfirmDialogModule,
+    DialogModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
